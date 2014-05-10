2c9345b46128aa8338ffc2de1ace2960b1ba50b2
Matt <mcgranam@gmail.com>
2014-05-10 07:34:13 -0400

    Project Euler Updates

--- projecteuler016.py
+++ projecteuler016.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # File name: 		projecteuler016.py
-# Author: 			Matt McGranaghan
+# Author: 		Matt McGranaghan
 # Date Created:		2014/04/25
 # Date Modified: 	2014/04/25
 # Python Version: 	2.7
