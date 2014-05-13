d3f4c2d3432e50158912b47b7a8660cde851a82b
Matt <mcgranam@gmail.com>
2014-05-13 07:18:06 -0400

    Project Euler Updates

--- projecteuler043.py
+++ projecteuler043.py
@@ -7,20 +7,23 @@
 # Python Version: 	2.7
 
 import itertools
-perms = [''.join(i) for i in itertools.permutations("0123456789")]
 
-sum_special = 0
-for p in perms:
-	if int(p[1:4]) % 2 == 0:
-		if int(p[2:5]) % 3 == 0:
-			if int(p[3:6]) % 5 == 0:
-				if int(p[4:7]) % 7 == 0:
-					if int(p[5:8]) % 11 == 0:
-						if int(p[6:9]) % 13 == 0:
-							if int(p[7:10]) % 17 == 0:
-								sum_special += int(p)
+def solution043():
+	perms = [''.join(i) for i in itertools.permutations("0123456789")]
 
-print sum_special
+	sum_special = 0
+	for p in perms:
+		if int(p[1:4]) % 2 == 0:
+			if int(p[2:5]) % 3 == 0:
+				if int(p[3:6]) % 5 == 0:
+					if int(p[4:7]) % 7 == 0:
+						if int(p[5:8]) % 11 == 0:
+							if int(p[6:9]) % 13 == 0:
+								if int(p[7:10]) % 17 == 0:
+									sum_special += int(p)
 
+	return sum_special
+
+print solution043()
 
 
